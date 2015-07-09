6bd031646627eb875c27face98b488736ea0ba94
Kent Mein <mein@cs.umn.edu>
2015-07-09 14:07:06 -0500

    Added a simple test skeleton.

---
+++ src/test.c
@@ -0,0 +1,48 @@
+/* 
+	A unit test should:
+	Set up all conditions for testing.
+	Call the method (or Trigger) being tested.
+	Verify that the results are correct.
+	Clean up modified records. (Not really necessary on Force.com.)
+*/
+
+struct testval {
+	int total;
+	int errors;
+};
+
+struct testval testres;
+
+int test_get_conf_name () {
+	char *name, *name2;
+	int x=0;
+
+	testres.total+= 2;
+
+	name = get_conf_name("lpc");
+	if (strcmp(name,".lpcrc") != 0) {
+		printf("test_get_conf_name: test1 expected \"%s\" got \"%s\"\n",
+			".lpcrc", name);
+		x++;
+	}
+	name2 = get_conf_name("sweet");
+	if (strcmp(name,".sweetrc") != 0) {
+		printf("test_get_conf_name: test2 expected \"%s\" got \"%s\"\n",
+			".sweetrc", name);
+		x++;
+	}
+
+	testres.total+= 2;
+	testres.errors+= x;
+
+	return x;
+}
+
+int main() {
+	if (!test_get_conf_name()) {
+		printf("test_get_conf_name: OK\n");
+	}
+
+	printf("Total tests: %s\n", testres.total);
+	printf("Total errors: %s\n", testres.errors);
+}
