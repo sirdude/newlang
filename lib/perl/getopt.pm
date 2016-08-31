ea306f6448de62f68781d0ca0ea97199acc8a4eb
Kent Mein <mein@cs.umn.edu>
2016-08-31 10:45:34 -0500

    Still needs work but fleshing it out a bit.

--- lib/perl/getopt.pm
+++ lib/perl/getopt.pm
@@ -11,9 +11,47 @@ our @EXPORT = qw(
 
 sub GetOptions {
 	my ($Options, @values) = @_;
+	my ($regex, $simple, $command, $var, $tmp);
 
 	foreach my $i (@values) {
-		print "$i\n";
+		$simple = 0;
+print "$i\n";
+		if ($i =~ /(.*)=i/) {
+			$var = $1;
+			$regex = "(.*)=%d";
+		
+		} elsif ($i =~ /(.*)=s/) {
+			$var = $1;
+			$regex = "((\"[^\"\]]*";
+
+		} elsif ($i =~ /(.*)=(.*)/) {
+			print "Error in format $i should be VAR=i or VAR=s\n";
+			return 0;
+		} else {
+			$regex = $i;
+			$simple = 1;
+		}
+
+		# XXX Need to fix this...
+		$command = $argv;
+
+		if ($command =~ /^(.*)\s+$regex\s+(.*)/) {
+			$tmp = $2; # Note this is in $regex
+		} elsif ($comamnd =~ /^$regex\s+(.*)/) {
+			$tmp = $1; # again in $regex
+		} elsif ($command =~ /(.*)\s+$regex/) {
+			$tmp = $2; # again in $regex
+		} elsif ($command =~ $regex) {
+			$tmp = $1;
+		}
+
+		if ($simple) {
+			$Options{$regex} = 1;
+		} else {
+			$Options{$var} = $tmp;
+		}
+
+		# XXX need to remove from env here...
 	}
 
 	return 1;
